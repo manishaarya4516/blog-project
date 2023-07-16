b1ec710a854a0f847f20b2338aa77958026c933e
manishaarya4516 <manishaarya4516@gmail.com>
2023-07-16 08:46:09 -0700

    update

--- src/component/sidebar/sidebar.js
+++ src/component/sidebar/sidebar.js
@@ -13,7 +13,7 @@ export const Sidebar = () => {
       />
        <p>
         Having a ‘About Me’ page on your website is crucial for your business so
-        that you can create a good first impression and build strong connections
+        that you can create  that a good first impression and build strong connections
         with your readers. An ‘About Me’ page should convey who you are, what
         your values and principles are, how you got to where you are, and why
         your product or service is important to you. If writing an ‘About Me’
